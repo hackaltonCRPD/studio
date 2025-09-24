74f40f8ec8f998bfe8561f2c0d0776530e54eef2
Abdoul Latief Tabally <atabally@crpgm.org>
2025-09-24 13:53:06 +0000

    make the animation a little bit slower, not very slow but such that the

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -119,8 +119,8 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'fade-in-down': 'fade-in-down 0.5s ease-out forwards',
-        'fade-in-slide-up': 'fade-in-slide-up 0.5s ease-out forwards',
+        'fade-in-down': 'fade-in-down 0.8s ease-out forwards',
+        'fade-in-slide-up': 'fade-in-slide-up 0.8s ease-out forwards',
       },
     },
   },
